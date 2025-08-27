ce839ab72a7cb5f6ae798f014e0a1ad7e0f1f850
mquesada-byte <maxquesadaguevara@gmail.com>
2025-08-27 16:55:49 -0600

    Update Prueba.py

--- Prueba.py
+++ Prueba.py
@@ -734,6 +734,11 @@ if st.session_state.get("step") == 3 and st.session_state.get("step3") == "C":
             st.session_state.step3 = "VAL"   # salto a Valoración del asesor
             st.rerun()
 
+    # 👇 Detiene el render aquí mientras sigas en el Paso 1
+    st.stop()
+
+
+
 
 # =========================
 # PASO 3VAL – Valoración del asesor
@@ -3544,6 +3549,7 @@ st.session_state["reporte"]["balance_general"] = {
     "patrimonio": int(round(patrimonio)),
     "capital_trabajo": int(round(capital_trabajo)),
 }
+
 
 
 
