29481242b521757bbf5e9abfd9e31f44c5409c44
mquesada-byte <maxquesadaguevara@gmail.com>
2025-08-28 10:48:34 -0600

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,14 +1,12 @@
 # Home.py
 import streamlit as st
-
 st.set_page_config(page_title="Evaluador de Capacidad de Pago", page_icon="💻")
 
 st.title("🏠 Evaluador de Capacidad de Pago")
-st.write("Usa el menú lateral o el enlace de abajo para iniciar con el recorrido")
+st.write("Usa el menú lateral o los enlaces de abajo para navegar por los pasos.")
 
 st.divider()
 
-# Datos del asesor
 with st.container(border=True):
     st.page_link(
         "pages/01_Asesor.py",
@@ -16,7 +14,6 @@ with st.container(border=True):
         help="Identificación del asesor, fecha/hora (CR) y geolocalización."
     )
 
-# Datos del cliente y su negocio
 with st.container(border=True):
     st.page_link(
         "pages/02_Cliente_y_negocio.py",   # <-- nombre con .py
@@ -24,6 +21,5 @@ with st.container(border=True):
         help="Datos del cliente/negocio para iniciar la evaluación."
     )
 
-
 st.divider()
-st.info("También puedes abrir los diferentes pasos desde el menú lateral.")
+st.info("También podés abrir los pasos desde el menú lateral.")
