e16bc49392a6db637a8eda332f1ee217b9d74355
mquesada-byte <maxquesadaguevara@gmail.com>
2025-08-28 10:47:07 -0600

    Update Home.py

--- Home.py
+++ Home.py
@@ -8,13 +8,22 @@ st.write("Usa el menú lateral o el enlace de abajo para iniciar con el recorrid
 
 st.divider()
 
-# ÚNICO paso habilitado por ahora
+# Datos del asesor
 with st.container(border=True):
     st.page_link(
         "pages/01_Asesor.py",
         label="➡️ 01 – Datos del asesor",
         help="Identificación del asesor, fecha/hora (CR) y geolocalización."
     )
+
+# Datos del cliente y su negocio
+with st.container(border=True):
+    st.page_link(
+        "pages/02_Cliente_y_negocio.py",   # <-- nombre con .py
+        label="➡️ 02 – Cliente y negocio",
+        help="Datos del cliente/negocio para iniciar la evaluación."
+    )
+
 
 st.divider()
 st.info("También puedes abrir los diferentes pasos desde el menú lateral.")
