b958cba2367ce2ea6bf1dfee20d4a628e949a61a
mquesada-byte <maxquesadaguevara@gmail.com>
2025-09-26 17:51:57 -0600

    Update 13_Balance_general.py

--- pages/13_Balance_general.py
+++ pages/13_Balance_general.py
@@ -170,7 +170,7 @@ if cliente_id and mes_iso:
         cursor.execute("""
             SELECT descripcion, monto, verificado, evidencia, comentario
             FROM balancegeneraldetalles
-            WHERE cliente_identificacion = ? AND mes_iso = ? AND seccion = 'inv_mp'
+            WHERE cliente_identificacion = ? AND mes_iso = ? AND seccion = 'inv_materia_prima'
         """, (cliente_id, mes_iso))
         rows = cursor.fetchall()
         conn.close()
@@ -297,7 +297,7 @@ with col2:
             cursor.execute("""
                 DELETE FROM balancegeneraldetalles
                 WHERE cliente_identificacion = ? AND mes_iso = ? 
-                  AND seccion IN ('caja_bancos','cxc_clientes','inv_mp')
+                  AND seccion IN ('caja_bancos','cxc_clientes','inv_materia_prima')
             """, (cliente_id, mes_iso))
 
             
