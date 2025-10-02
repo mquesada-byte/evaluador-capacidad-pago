ae55241c5a61b453dc289f06df8fe97bb59854e8
mquesada-byte <maxquesadaguevara@gmail.com>
2025-10-02 08:45:32 -0600

    Update 04_Ventas_botton_up.py

--- pages/04_Ventas_botton_up.py
+++ pages/04_Ventas_botton_up.py
@@ -33,53 +33,29 @@ def init_paso4_state(cliente_id: str, mes_iso: str):
     st.session_state.setdefault("no_data", False)
     vbu = st.session_state.ventas_bottomup
 
+
     # Buscar registro existente en la BD
-    #datos = load_visita(cliente_id)
-    #if datos and "ventas_bottomup" in datos:
-    #    db_row = datos["ventas_bottomup"]
-    
-    #    if db_row.get("mes_iso") == mes_iso:
-    #        if db_row.get("no_data") == 1:
-    #            st.session_state.no_data = True
-    #        else:
-    #            st.session_state.no_data = False
-
-    #        vbu["unidad_clientes"] = db_row.get("unidad_clientes") or "Mes"
-    #        vbu["clientes"] = db_row.get("clientes_valor") or 0
-    #        vbu["dias_abiertos"] = db_row.get("dias_abiertos") or 0
-    #        vbu["semanas_abiertas"] = db_row.get("semanas_abiertas") or 0
-    #        vbu["ticket_promedio"] = db_row.get("ticket_promedio_colones") or 0
-    #        vbu["comentario"] = db_row.get("comentario") or ""
-    #    else:
-    #        # Inicializar por defecto
-    #        _init_defaults(vbu)
-    #else:
-    #    _init_defaults(vbu)
-
-
     datos = load_visita(cliente_id)
     if datos and "ventas_bottomup" in datos:
-        db_vbu = datos["ventas_bottomup"]
+        db_row = datos["ventas_bottomup"]
     
-        if db_vbu.get("mes_iso") == mes_iso:
-            st.session_state.no_data = (db_vbu.get("no_data") == 1)
-    
-            vbu["unidad_clientes"] = db_vbu.get("unidad_clientes") or "Mes"
-            vbu["clientes"] = int(db_vbu.get("clientes_valor") or 0)
-            vbu["dias_abiertos"] = int(db_vbu.get("dias_abiertos") or 0)
-            vbu["semanas_abiertas"] = int(db_vbu.get("semanas_abiertas") or 0)
-            vbu["ticket_promedio"] = int(db_vbu.get("ticket_promedio_colones") or 0)
-            vbu["comentario"] = db_vbu.get("comentario") or ""
+        if db_row.get("mes_iso") == mes_iso:
+            if db_row.get("no_data") == 1:
+                st.session_state.no_data = True
+            else:
+                st.session_state.no_data = False
+
+            vbu["unidad_clientes"] = db_row.get("unidad_clientes") or "Mes"
+            vbu["clientes"] = db_row.get("clientes_valor") or 0
+            vbu["dias_abiertos"] = db_row.get("dias_abiertos") or 0
+            vbu["semanas_abiertas"] = db_row.get("semanas_abiertas") or 0
+            vbu["ticket_promedio"] = db_row.get("ticket_promedio_colones") or 0
+            vbu["comentario"] = db_row.get("comentario") or ""
         else:
+            # Inicializar por defecto
             _init_defaults(vbu)
     else:
         _init_defaults(vbu)
-
-
-
-
-
-
 
 
 
