e6f3b3ebf7e5e0981b79a37d21c621d67f8493a6
mquesada-byte <maxquesadaguevara@gmail.com>
2026-09-25 10:29:00 -0600

    Update 19_Hechos_relevantes.py

--- pages/19_Hechos_relevantes.py
+++ pages/19_Hechos_relevantes.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(
     page_title="Paso 19: Hechos relevantes",
     page_icon="📝",
-    layout="wide"
+    #layout="wide"
 )
 
 st.title("📝 Paso 19: Hechos relevantes")
